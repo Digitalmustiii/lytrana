4682d6d6eba7932bf0ec626d0e517c9f7d4cede7
Digitalmustiii <sanusimustapha387@gmail.com>
2025-06-07 00:52:00 +0100

    lol

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  
-  images: { 
-    domains: ['images.unsplash.com'] 
-  },
-};
+const nextConfig: NextConfig = {}
 
-export default nextConfig;
+export default nextConfig
